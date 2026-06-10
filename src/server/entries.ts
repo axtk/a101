b298511157bfb30df77c07a157f63509b5af0837
axtk <axtkx@inbox.ru>
2026-06-11 00:39:48 +0300

    update shape config

--- src/server/entries.ts
+++ src/server/entries.ts
@@ -2,10 +2,10 @@
 // Ignore this file if a custom set of entry exports is required.
 export const entries = (
   await Promise.all([
-    import("../entries/blank-demo/server.ts"),
-    import("../entries/files-demo/server.ts"),
     import("../entries/main/server.ts"),
-    import("../entries/react-demo/server/index.ts"),
+    import("../entries/files-demo/server.ts"),
+    import("../entries/blank-demo/server.ts"),
     import("../entries/react-demo-typed-routes/server/index.ts"),
+    import("../entries/react-demo/server/index.ts"),
   ])
 ).map(({ server }) => server);
